56645c57a82e70360e273e7c5c4d3a987004908d
PSNY-23 <146502007+PSNY-23@users.noreply.github.com>
2024-05-16 21:37:22 +0530

    Add files via upload
    The website was not looking good, at least as I wanted it to be so I have made a better interface and have done little modification with the emojis and the next button.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let randomfacts = async () => {
   let response = await fetch("https://dog-api.kinduff.com/api/facts");
   let data = await response.json();
+
   return data.facts;
 };
 
@@ -12,6 +13,7 @@ async function displayFacts() {
 let randomimg = async () => {
   let response = await fetch("https://dog.ceo/api/breeds/image/random");
   let data = await response.json();
+
   return data.message;
 };
 
@@ -19,3 +21,37 @@ async function displayDogImage() {
   let path = document.getElementById("dogimg");
   path.src = await randomimg();
 }
+let react = true;
+function finalFunction() {
+  if (react) {
+    displayFacts();
+    displayDogImage();
+    react = false;
+    document.getElementById("button").innerText = "Please choose emoji";
+  }
+}
+
+// let emojiButton1 = document.getElementById("emoji1");
+// let emojiButton2 = document.getElementById("emoji2");
+// let emojiButton3 = document.getElementById("emoji3");
+// let emojiButton4 = document.getElementById("emoji4");
+// let emojiButton5 = document.getElementById("emoji5");
+function reacted() {
+  react = true;
+  document.getElementById("button").innerText = "Next";
+}
+
+async function translator() {
+  const res = await fetch("https://libretranslate.com/translate", {
+    method: "POST",
+    body: JSON.stringify({
+      q: "Hello!",
+      source: "en",
+      target: "es",
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  console.log(await res.json());
+}
+translator();
